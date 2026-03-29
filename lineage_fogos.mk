1e29c5a32605659918c94adc66466d8856102b5e
Byben <heybyben@gmail.com>
2026-03-29 14:30:06 +0700

    fogos: Axion!
    Change-Id: I9e799d2c61834fdfbc5349c948e2e1bbb271919b

--- lineage_fogos.mk
+++ lineage_fogos.mk
@@ -26,3 +26,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="fogos_g-user 15 V1UGS35M.75-14-9-3-1-2 9dc92 release-keys" \
     BuildFingerprint=motorola/fogos_g/fogos:15/V1UGS35M.75-14-9-3-1-2/9dc92:user/release-keys \
     DeviceProduct=fogos_g
+
+# Axion Stuff
+TARGET_ENABLE_BLUR := true
+AXION_CAMERA_REAR_INFO := 50,2
+AXION_CAMERA_FRONT_INFO := 16
+AXION_MAINTAINER := Byben
+AXION_PROCESSOR := Snapdragon_6s_Gen_3
